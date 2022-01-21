b748e4cb7fc9878895406375a51b46d6870826e9
MadPlayer <kch9001@gmail.com>
2022-01-22 00:28:20 +0900

    open socket

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -13,6 +13,16 @@ int main(int argc, char *argv[])
   io_context context;
   tcp::endpoint ep(make_address(address), 80);
   tcp::socket socket(context);
+  std::error_code ec;
+
+
+  socket.open(tcp::v4(), ec);
+
+  if (ec.value())
+    {
+      std::cerr << ec.value() << " " << ec.message() << std::endl;
+      return ec.value();
+    }
 
   return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -11,8 +11,19 @@ int main(int argc, char *argv[])
 {
   io_context context;
   tcp::endpoint ep(address_v4::any(), 3333);
-  tcp::socket socket(context);
+  tcp::acceptor socket(context);
+  std::error_code ec;
 
+  socket.open(tcp::v4(), ec);
+
+  if (ec.value())
+    {
+      std::cerr
+        << "Failed to open the acceptor socket!\n"
+        << "Error code: " << ec.value()
+        << ".\n " << ec.message() << std::endl;
+      return ec.value();
+    }
 
   return 0;
 }
