812d2c17d26c104d3c74698655922006cd96dabb
MadPlayer <kch9001@gmail.com>
2022-01-26 00:13:06 +0900

    add asio thread pool

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -3,9 +3,46 @@
 #include <string>
 #include <system_error>
 #include <array>
+#include <memory>
 
 using namespace asio;
 using namespace asio::ip;
+
+thread_pool pool(10);
+
+void work(tcp::socket* sock)
+{
+  streambuf buf;
+  read_until(*sock, buf, '\n');
+  std::string msg;
+  std::istream input(&buf);
+
+  std::getline(input, msg);
+  std::cout << msg << std::endl;
+}
+
+struct safe_socket
+{
+  std::unique_ptr<io_context> io_ptr;
+  std::unique_ptr<tcp::socket> socket_ptr;
+
+  safe_socket():
+    io_ptr(std::make_unique<io_context>()),
+    socket_ptr(std::make_unique<tcp::socket>(*io_ptr)) {}
+
+  safe_socket(safe_socket&& s)
+  {
+    io_ptr = std::move(s.io_ptr);
+    socket_ptr = std::move(s.socket_ptr);
+  }
+
+  tcp::socket &
+  get_socket()
+  {
+    return *socket_ptr;
+  }
+};
+
 
 int main(int argc, char *argv[])
 {
@@ -21,18 +58,15 @@ int main(int argc, char *argv[])
 
       while (true)
         {
-          io_context io;
-          tcp::socket sock(io);
-          acceptor.accept(sock);
+          safe_socket s;
+          acceptor.accept(s.get_socket());
           std::cout << "accept\n";
-          streambuf buf;
-          read_until(sock, buf, '\n');
-          std::string msg;
-          std::istream input(&buf);
 
-          std::getline(input, msg);
-          std::cout << msg << std::endl;
+          asio::post(pool, [socket = std::move(s)](){
+            work(socket.socket_ptr.get());
+          });
         }
+
       acceptor.close();
       std::cout << "closed\n";
     }
@@ -41,6 +75,9 @@ int main(int argc, char *argv[])
       std::cerr << "Error(" << ec.code() << "): " << ec.what() << std::endl;
       return ec.code().value();
     }
+
+  pool.stop();
+  pool.join();
 
   return 0;
 }
